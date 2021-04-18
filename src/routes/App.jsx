13e5ecbe8af4590d7a9245cd8563603af158cfa1
jucetina <juliethcetina@gmail.com>
2021-04-17 23:21:50 -0500

    Se modifican las rutas y se actualiza un snapshot

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route exact path="platzi-store-jest" component={Home} />
+        <Route exact path="/platzi-store-jest" component={Home} />
         <Route exact path="/checkout" component={Checkout} />
         <Route component={NotFound} />
       </Switch>
